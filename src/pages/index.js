37eb0d71ad186d2cfc0ee2fd6f385bc869c7b893
hm <hm.dev7@gmail.com>
2019-01-19 21:31:55 +0700

    Try Bulma

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
-import Layout from '../components/HOC/Layout';
-import SEO from '../components/Utility/SEO';
+import Layout from '../components/HOC/Layout'
+import SEO from '../components/Utility/SEO'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Welcome To My Gatsby Blog!</h1>
+    <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
+    <section className='hero is-fullheight is-primary'>
+      <div className='hero-body'>
+        <div className='container has-text-centered'>
+          <h1 className='title'>Aflasio</h1>
+          <h1 className='subtitle'>Welcome To My Gatsby Site!</h1>
+        </div>
+      </div>
+    </section>
   </Layout>
 )
 

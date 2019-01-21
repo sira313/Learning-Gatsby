b9fbf6e22849ad757abe057d15d51a2471700fe4
hm <hm.dev7@gmail.com>
2019-01-21 14:16:46 +0700

    Add menu items in siteMetadata

--- aflasio.js
+++ aflasio.js
@@ -1,6 +1,14 @@
+menuItems = [
+  { name: 'Commisioned', href: '#' },
+  { name: 'Random Artwork', href: '#' },
+  { name: 'Archives', href: '#' },
+  { name: 'About', href: '#' }
+]
+
 exports.siteMetadata = {
   title: `Aflasio`,
   description: `Portfolio of a FLOSS user artist`,
   author: `@sira313`,
-  primaryColor: `#293e53`
+  primaryColor: `#293e53`,
+  menuItems
 }

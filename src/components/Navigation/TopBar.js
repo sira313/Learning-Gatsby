96df38b9be73f6cc5a7b89b446500d836b18a78b
hm <hm.dev7@gmail.com>
2019-01-22 10:57:28 +0700

    Query menuItems from siteMetadata directly here

--- src/components/Navigation/TopBar.js
+++ src/components/Navigation/TopBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'gatsby'
+import { graphql, StaticQuery, Link } from 'gatsby'
 import NavigationItem from './NavigationItem'
 import image from '../../images/aflasiowhite.png'
 
@@ -7,34 +7,54 @@ import image from '../../images/aflasiowhite.png'
  * shown: is state of menu item on mobile
  * onToggled: is a handler for burger to toggling menu item on mobile
  */
-const TopBar = ({ menuItems, shown, onToggled }) => (
-  <nav className='navbar is-dark' role='navigation' aria-label='main navigation'>
-    <div className='container'>
-      <div className='navbar-brand'>
-        <Link className='navbar-item' to='/'>
-          <img alt='Portfolio of a FLOSS user artist' src={image} />
-        </Link>
+const TopBar = ({ shown, onToggled }) => {
+  const el = data => (
+    <nav
+      className='navbar is-fixed-top is-dark'
+      role='navigation'
+      aria-label='main navigation'>
+      <div className='container'>
+        <div className='navbar-brand'>
+          <Link className='navbar-item' to='/'>
+            <img alt='Portfolio of a FLOSS user artist' src={image} />
+          </Link>
 
-        <div
-          role='button'
-          onClick={onToggled}
-          className='navbar-burger burger'
-          aria-label='menu'
-          aria-expanded='false'
-          data-target='navItem'>
-          <span aria-hidden='true' />
-          <span aria-hidden='true' />
-          <span aria-hidden='true' />
+          <div
+            role='button'
+            onClick={onToggled}
+            className='navbar-burger burger'
+            aria-label='menu'
+            aria-expanded='false'
+            data-target='navItem'>
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+            <span aria-hidden='true' />
+          </div>
+        </div>
+
+        <div id='navItem' className={`navbar-menu${shown ? ' is-active' : ''}`}>
+          <div className='navbar-end'>
+            <NavigationItem items={data.site.siteMetadata.menuItems} />
+          </div>
         </div>
       </div>
+    </nav>
+  )
 
-      <div id='navItem' className={`navbar-menu${shown ? ' is-active' : ''}`}>
-        <div className='navbar-end'>
-          <NavigationItem items={menuItems} />
-        </div>
-      </div>
-    </div>
-  </nav>
-)
+  return <StaticQuery query={query} render={el} />
+}
+
+const query = graphql`
+  query {
+    site {
+      siteMetadata {
+        menuItems {
+          name
+          href
+        }
+      }
+    }
+  }
+`
 
 export default TopBar

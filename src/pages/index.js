eab9f8c87f2361c61b83b57846d301f34d07b6b3
Sira Argia <si.ecenk@gmail.com>
2019-01-22 22:08:20 +0700

    custom landing page with 2 column

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Layout from '../components/HOC/Layout'
 import SEO from '../components/Utility/SEO'
 import TopBar from '../components/Navigation/TopBar'
 import logo from '../images/aflasiodark.png'
+import landingPage from '../images/landingPage.svg'
 
 const IndexPage = () => (
   <Layout>
@@ -13,8 +14,25 @@ const IndexPage = () => (
       </div>
       <div className='hero-body'>
         <div className='container has-text-centered'>
-          <h1 className='title'>Aflasio</h1>
-          <h1 className='subtitle'>Welcome To My Gatsby Site!</h1>
+          <div className='columns is-vcentered'>
+            <div className='column is-5'>
+              <figure className='image is-4by3'>
+                <img src={landingPage} alt='Aflasio' />
+              </figure>
+            </div>
+            <div className='column is-6 is-offset-1'>
+              <h1 className='title is-2'>
+                Hello...
+              </h1>
+              <h2 className='subtitle is-4'>
+                My name is Sira Argia. I am an artist who use FLOSS for work.
+              </h2>
+              <br />
+              <p className='button is-medium is-dark is-outlined'>
+                More...
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </section>

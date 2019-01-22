96078cb07f1e4adfbaaf622df812eac4edfdcf8f
hm <hm.dev7@gmail.com>
2019-01-22 10:58:29 +0700

    Remove query menuItems and implement sticky navbar on scroll up

--- src/components/HOC/Layout.js
+++ src/components/HOC/Layout.js
@@ -1,52 +1,42 @@
 import React, { Component } from 'react'
-import { graphql, StaticQuery } from 'gatsby'
 import TopBar from '../Navigation/TopBar'
+import { addOneScroll, removeOneScroll, status } from '../../shared/one-scroll'
 
 class Layout extends Component {
   state = {
-    isMenuShown: false
+    isMenuShown: false,
+    isNavShown: false
   }
 
   burgerHandler = () => {
     this.setState(state => ({ isMenuShown: !state.isMenuShown }))
   }
 
+  componentDidMount () {
+    addOneScroll(s => {
+      this.setState({ isNavShown: s === status.UP })
+    })
+  }
+
+  componentWillUnmount () {
+    removeOneScroll()
+  }
+
   render () {
-    const { isNavShown = true, children } = this.props
-    const { isMenuShown } = this.state
-    const el = data => {
-      const meta = data.site.siteMetadata
-      return (
-        <>
-          {isNavShown ? (
-            <header>
-              <TopBar
-                menuItems={meta.menuItems}
-                onToggled={this.burgerHandler}
-                shown={isMenuShown}
-              />
-            </header>
-          ) : null}
-          <main>{children}</main>
-          <footer>{/* TODO: Add footer */}</footer>
-        </>
-      )
-    }
-    return <StaticQuery query={query} render={el} />
+    const { children } = this.props
+    const { isNavShown, isMenuShown } = this.state
+    return (
+      <>
+        {isNavShown ? (
+          <header>
+            <TopBar onToggled={this.burgerHandler} shown={isMenuShown} />
+          </header>
+        ) : null}
+        <main>{children}</main>
+        <footer>{/* TODO: Add footer */}</footer>
+      </>
+    )
   }
 }
-
-const query = graphql`
-  query {
-    site {
-      siteMetadata {
-        menuItems {
-          name
-          href
-        }
-      }
-    }
-  }
-`
 
 export default Layout

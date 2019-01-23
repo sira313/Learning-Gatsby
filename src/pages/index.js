8b148b6135cd07ec764e3b767513d8828e80e853
Sira Argia <si.ecenk@gmail.com>
2019-01-23 16:06:14 +0700

    add background to hero

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import Layout from '../components/HOC/Layout'
 import SEO from '../components/Utility/SEO'
 import TopBar from '../components/Navigation/TopBar'
-import logo from '../images/aflasiodark.png'
+import logo from '../images/aflasiowhite.png'
 import landingPage from '../images/landingPage.svg'
 import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <Layout>
     <SEO title='Home' keywords={[`gatsby`, `application`, `react`]} />
-    <section className='hero is-fullheight is-light'>
+    <section className='hero is-fullheight has-bg-img is-dark'>
       <div className='hero-head'>
         <TopBar logoSrc={logo} />
       </div>
@@ -17,31 +17,23 @@ const IndexPage = () => (
         <div className='container has-text-centered'>
           <div className='columns is-vcentered'>
             <div className='column is-5'>
-              <figure className='image is-4by3 animated slower infinite heartbeat'>
+              <figure className='image is-4by3'>
                 <img src={landingPage} alt='Aflasio' />
               </figure>
             </div>
             <div className='column is-6 is-offset-1'>
               <h1 className='title is-2'>
-                Hello...
+                Aflasio
               </h1>
               <h2 className='subtitle is-4'>
-                My name is Sira Argia. I am an artist who use FLOSS for work.
+                Hello.. My name is Sira Argia. I am an artist who use FLOSS for work.
               </h2>
               <br />
-              <Link to='/' className='button is-medium is-dark is-outlined'>
+              <Link to='/' className='button is-medium is-light is-outlined'>
                 More...
               </Link>
             </div>
           </div>
-        </div>
-      </div>
-    </section>
-    <section className='hero is-fullheight'>
-      <div className='hero-body'>
-        <div className='container has-text-centered'>
-          <h1 className='title'>My Latest Project</h1>
-          <h1 className='subtitle'>Here you can see my latest projects!</h1>
         </div>
       </div>
     </section>

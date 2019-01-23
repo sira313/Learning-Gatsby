8faf06cbe5e7f10c4c8c7e44c57678d921ae21ab
Sira Argia <45110036+sira313@users.noreply.github.com>
2019-01-23 14:23:54 +0700

    remove animation on landing picture

--- src/pages/index.js
+++ src/pages/index.js
@@ -17,7 +17,7 @@ const IndexPage = () => (
         <div className='container has-text-centered'>
           <div className='columns is-vcentered'>
             <div className='column is-5'>
-              <figure className='image is-4by3 animated slower infinite heartbeat'>
+              <figure className='image is-4by3'>
                 <img src={landingPage} alt='Aflasio' />
               </figure>
             </div>

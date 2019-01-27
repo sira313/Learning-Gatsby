d35dbde6c4ffb796ae0b9b36ff16f4b22e7fa881
hm <hm.dev7@gmail.com>
2019-01-27 16:48:39 +0700

    Enabled gallery post template

---
+++ src/templates/gallery-post.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import { graphql } from 'gatsby'
+
+class GalleryPost extends Component {
+  render () {
+    const { data } = this.props
+    const post = data.markdownRemark
+    return (
+      <div className='section'>
+        <div className='container'>
+          <div
+            className='content is-size-5-desktop is-size-5-tablet has-text-justified'
+            dangerouslySetInnerHTML={{ __html: post.html }}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export const pageQuery = graphql`
+  query galleryPost($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      id
+      excerpt(pruneLength: 160)
+      html
+      frontmatter {
+        title
+        date(formatString: "MMMM DD, YYYY")
+      }
+    }
+  }
+`
+
+export default GalleryPost

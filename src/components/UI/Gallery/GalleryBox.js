e1fe4bc5731f8489022607a0eb8d9e05c7e366c4
Sira Argia <si.ecenk@gmail.com>
2019-01-28 23:11:24 +0700

    custom box shadow

--- src/components/UI/Gallery/GalleryBox.js
+++ src/components/UI/Gallery/GalleryBox.js
@@ -4,7 +4,7 @@ import { Link } from 'gatsby'
 // TODO: Change link by post slug
 const GalleryBox = props => (
   <div className='column is-one-quarter-fullhd is-one-third-desktop is-half-tablet'>
-    <div className='box has-shadow-hover' style={{ padding: '0.5rem' }}>
+    <div className='box has-box-shadow has-shadow-hover' style={{ padding: '0.5rem' }}>
       <Link
         className='image'
         to='/gallery/stylized-portrait-tribute-to-razan-al-najjar/'>

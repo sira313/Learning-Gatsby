2f7de6227149c40cd99b62492634f9d89bc4f18a
Sira Argia <si.ecenk@gmail.com>
2019-02-25 21:05:53 +0700

    update 404

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,22 @@ import SEO from '../components/Utility/SEO'
 const NotFoundPage = () => (
   <Layout>
     <SEO title='404: Not found' />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <body>
+      <section className='hero is-light is-fullheight'>
+        <div className='hero-body'>
+          <div className='container has-text-centered'>
+            <div className='column is-6 is-offset-3'>
+              <h1 className='title'>
+                Coming Soon
+              </h1>
+              <h2>
+                This page is under construction.
+              </h2>
+            </div>
+          </div>
+        </div>
+      </section>
+    </body>
   </Layout>
 )
 

a5792418b5c152a9bfa87073e707ff604d4754c3
hm <hm.dev7@gmail.com>
2019-03-12 17:20:04 +0700

    Add banner props

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -5,19 +5,29 @@ import SEO from '../components/Utility/SEO'
 import TopBar from '../components/Navigation/TopBar'
 import logo from '../images/aflasiowhite.png'
 import BlogItems from '../components/UI/Blog/BlogItems'
-import Banner from '../components/UI/Banner';
+import Banner from '../components/UI/Banner'
 
 const seoKeywords = ['aflasio', 'blog', 'linux']
 
 const Blog = ({ data }) => {
+  const { edges } = data.blog
+  // categories is automatic created by post category distinctly
+  const categories = edges
+    .map(e => e.node.frontmatter.category)
+    .filter((v, i, e) => e.indexOf(v) === i)
+
+  function onSearch (e) {
+    console.log('SEARCH FOR: ', e)
+  }
+
   return (
     <Layout>
       <SEO title='Blog' keywords={seoKeywords} />
       <TopBar className='is-dark' logoSrc={logo} />
       <section className='section has-background-light'>
         <div className='container'>
-          <Banner />
-          <BlogItems edges={data.blog.edges} />
+          <Banner categories={categories} onSearch={onSearch} />
+          <BlogItems edges={edges} />
         </div>
       </section>
     </Layout>

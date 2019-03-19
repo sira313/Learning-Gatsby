3413c1c34a74c393b3ed1f7955dd2343a4f4417c
Sira Argia <si.ecenk@gmail.com>
2019-03-19 22:34:03 +0700

    set up box padding for desktop and mobile

--- src/components/UI/Gallery/GalleryItem.js
+++ src/components/UI/Gallery/GalleryItem.js
@@ -5,8 +5,7 @@ import Img from 'gatsby-image'
 const GalleryItem = ({ node }) => (
   <div className='column is-one-quarter-fullhd is-one-third-desktop is-half-mobile is-gap-mobile'>
     <div
-      className='box has-box-shadow has-shadow-hover'
-      style={{ padding: '0.5rem' }}>
+      className='box has-box-shadow has-shadow-hover has-desktop-padding has-mobile-padding'>
       <Link className='image' to={node.fields.slug}>
         <Img fluid={node.frontmatter.coverIndex.childImageSharp.fluid} />
       </Link>
